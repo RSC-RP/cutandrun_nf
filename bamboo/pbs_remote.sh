4bef9c572e997461d14ddbc8d9822740815a321d
jsmi26 <jenny.smith@seattlechildrens.org>
2023-12-21 11:31:37 -0800

    added bash shell options

--- bamboo/pbs_remote.sh
+++ bamboo/pbs_remote.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 module load pbspro/2020.1
 
+set -u -o pipefail
+
 # query loop logic from: https://stackoverflow.com/questions/25674240/how-to-know-when-pbs-batch-jobs-are-complete
 numJobs=0
 # jobid=$(~/tmp/pbs_wrapper.sh ~/tmp/knitRForBiols.pbs)
@@ -19,7 +21,7 @@ jobid=$(qsub -v TEMP_DIR="$2" $1)
 echo "job: $jobid"
 echo "PBS Script: $1"
 echo "TEMP_DIR: $2"
-(( numJobs++ ))
+(( ++numJobs ))
 numDone=0
 while [ $numDone -lt $numJobs ]; do
   if qstat -xf $jobid | grep 'Exit_status = '
