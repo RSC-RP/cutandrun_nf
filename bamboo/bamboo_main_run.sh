139dc2f03496341f0c401f8c62c38b3f2d5328ab
jsmi26 <jenny.smith@seattlechildrens.org>
2023-12-22 14:46:31 -0800

    bash interpreter with login shell flag; parse commandline arguements in run script

--- bamboo/bamboo_main_run.sh
+++ bamboo/bamboo_main_run.sh
@@ -2,23 +2,38 @@
 
 set -eu
 DATE=$(date +%F)
-NFX_CONFIG=../nextflow_test.config
+NFX_CONFIG=./nextflow_test.config
 #Options: 'PBS_apptainer','local_apptainer','local_singularity', 'PBS_singularity'
 NFX_PROFILE='local_apptainer'
 #Options: 'bowtie2_index_only', 'align_call_peaks', 'call_peaks'
 NFX_ENTRY='align_call_peaks'
-#The output prefix on filenames for reports/logs
-REPORT=${1:-"pipeline_report"}
+
+# REPORT is the output prefix on filenames for reports/logs
+# if 1st positional argument is a command line flag for nextflow run, then use the default report name
+ALL_ARGS="$@"
+LEN="$#"
+if [ $(echo "$ALL_ARGS" | grep -Ec "^-{1,2}[a-zA-Z]") == 0 ] && [ $LEN -le 1 ]
+then
+    REPORT=${1:-"pipeline_report"}
+    ARGS=''
+elif [ $(echo "$ALL_ARGS" | grep -Ec "^-{1,2}[a-zA-Z]") != 0 ]
+then
+    REPORT="pipeline_report"
+    ARGS=$ALL_ARGS
+else
+    REPORT=${@:1:1}
+    ARGS=${@:2:$LEN}
+fi
 
 # Nextflow run to execute the workflow 
 PREFIX=${REPORT}_${DATE}
 nextflow -c ${NFX_CONFIG}\
     -log artifacts/reports/${PREFIX}_nextflow.log \
     run main.nf \
+    $ARGS \
     -entry ${NFX_ENTRY} \
     -profile ${NFX_PROFILE} \
     -with-report artifacts/reports/${PREFIX}.html \
     -with-dag artifacts/dag/${PREFIX}_dag.pdf \
     -cache TRUE \
-    -resume \
-    "$@"
+    -resume
