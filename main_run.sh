22bc07852c9e8ee8e2a8069e08cddebaa3bd1e35
jsmi26 <jenny.smith@seattlechildrens.org>
2024-01-16 13:21:24 -0800

    added singularity clean cache

--- main_run.sh
+++ main_run.sh
@@ -4,7 +4,7 @@ set -eu
 DATE=$(date +%F)
 NFX_CONFIG=./nextflow.config
 #Options: 'PBS_apptainer','local_apptainer','local_singularity', 'PBS_singularity'
-NFX_PROFILE='local_apptainer'
+NFX_PROFILE='PBS_singularity'
 #Options: 'bowtie2_index_only', 'align_call_peaks', 'call_peaks'
 NFX_ENTRY='align_call_peaks'
 #The output prefix on filenames for reports/logs
@@ -14,6 +14,8 @@ REPORT=${1:-"pipeline_report"}
 if [[ $NFX_PROFILE =~ "singularity" ]]
 then
     module load singularity
+    singularity --version
+    singularity cache clean --force
 fi
 
 if [[ $NFX_PROFILE =~ "apptainer" ]]
@@ -23,7 +25,7 @@ then
     # This actually appears to be the issue in ~/.apptainer for the layers
     # Run 1x with this empty, the pull command fails
     # But if you don't remove/clean the cache files and Run 2nd time - pipeline is successful
-    # apptainer cache clean --force
+    apptainer cache clean --force
 fi
 
 # openssl s_client -brief -connect repo.anaconda.com:443 -servername cybertron
@@ -38,10 +40,10 @@ fi
 
 # Setting these environment variables has no effect. FATAL:   While making image from oci registry: error fetching image to cache: while building SIF from layers: conveyor failed to get: no descriptor found for reference "3c986513543ace0d0456d51f4a5e4c254065fa665b47f7ed2fe01ed23e406608"
 # not a solution with Nextflow v23.10 stable and v23.12.0-edge
-export SINGULARITY_MKSQUASHFS_PROCS=2
-export APPTAINER_MKSQUASHFS_PROCS=2
-echo "set SINGULARITY_MKSQUASHFS_PROCS = $SINGULARITY_MKSQUASHFS_PROCS"
-echo "set APPTAINER_MKSQUASHFS_PROCS = $APPTAINER_MKSQUASHFS_PROCS"
+# export SINGULARITY_MKSQUASHFS_PROCS=2
+# export APPTAINER_MKSQUASHFS_PROCS=2
+# echo "set SINGULARITY_MKSQUASHFS_PROCS = $SINGULARITY_MKSQUASHFS_PROCS"
+# echo "set APPTAINER_MKSQUASHFS_PROCS = $APPTAINER_MKSQUASHFS_PROCS"
 
 # Nextflow run to execute the workflow 
 PREFIX=${REPORT}_${DATE}
